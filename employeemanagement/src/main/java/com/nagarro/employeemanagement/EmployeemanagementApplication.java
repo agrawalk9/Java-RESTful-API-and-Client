d792be28e4c1e81bf0816c6928fbf92791293aae
kartikayagarwal <kartikayagarwal@kartikayagarwal.Nagarro.local>
2019-08-02 10:17:52 +0530

    With Spring Cloud Eureka Server

--- employeemanagement/src/main/java/com/nagarro/employeemanagement/EmployeemanagementApplication.java
+++ employeemanagement/src/main/java/com/nagarro/employeemanagement/EmployeemanagementApplication.java
@@ -1,21 +1,19 @@
 package com.nagarro.employeemanagement;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.nagarro.employeemanagement.Repository.EmployeeRepository;
-import com.nagarro.employeemanagement.model.Employee;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmployeemanagementApplication {
 
-	private static final Logger log= LoggerFactory.getLogger(EmployeemanagementApplication.class);
+	//private static final Logger log= LoggerFactory.getLogger(EmployeemanagementApplication.class);
 
+	@Value("${service.instance.name}")
+	private String instance;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagementApplication.class, args);
 		System.out.println("Hello World");
--- employeemanagement/src/main/java/com/nagarro/employeemanagement/rest/EmployeeManagementResource.java
+++ employeemanagement/src/main/java/com/nagarro/employeemanagement/rest/EmployeeManagementResource.java
@@ -1,6 +1,5 @@
 package com.nagarro.employeemanagement.rest;
 
-import java.awt.PageAttributes.MediaType;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
